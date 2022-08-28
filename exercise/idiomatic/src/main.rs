4c4f935809dec8bb5215dd163e982dbe1906b163
Sam M W <you@example.com>
2022-08-28 20:56:16 +0100

    silence clippy

--- exercise/idiomatic/src/main.rs
+++ exercise/idiomatic/src/main.rs
@@ -8,19 +8,15 @@
 
 // Challenge: Clippy doesn't find *everything*. What else would you change to make this code better?
 
-const pi: f32 = 3.14159265358979323846;
-
 fn count_to_5() -> i32 {
-    let mut foo = 0;
+    let mut count = 0;
     loop {
-        if foo > pi as i32 {
-            if foo > 5 {
-                break;
-            }
+        if count > std::f32::consts::PI as i32 {
+            break;
         }
-        foo = foo + 1;
+        count += 1;
     }
-    return 5;
+    5
 }
 
 fn main() {
