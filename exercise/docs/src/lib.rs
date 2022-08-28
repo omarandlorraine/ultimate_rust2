b8a2d12a142608279b430716f6658ed165ff569c
Sam M W <you@example.com>
2022-08-28 23:07:02 +0100

    document BURNT_ORANGE

--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -41,6 +41,8 @@ impl Pumpkin {
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
 
+/// This constant defines the orangeness of a pumpkin. It is a valid value for the `orangeness`
+/// field of the [`Pumpkin`] struct.
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
